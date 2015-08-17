38cff6c2e1094f0f2c2919736a69d8d154d9a4a3
byGeek01 <hide_69ch@yahoo.co.jp>
2015-08-17 16:35:40 +0900

    Initial Commit

---
+++ LunchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LunchApp
+//
+//  Created by hide on 2015/08/17.
+//  Copyright (c) 2015年 byGeek01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
