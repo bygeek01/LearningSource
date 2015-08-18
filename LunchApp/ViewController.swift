c43961c373a892e26dcc37c26696c73aeb013998
byGeek01 <hide_69ch@yahoo.co.jp>
2015-08-18 13:41:08 +0900

    first commit

--- LunchApp/ViewController.swift
+++ LunchApp/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegateFlowLayout,UICollectionViewDelegate {
+    
+    
+    @IBOutlet weak var menuNavCollectionView: MenuNavCollectionView!
+    
+    //初期化処理
+    //section1のセルを選択状態に設定
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        menuNavCollectionView.delegate = self
+        menuNavCollectionView.selectItemAtIndexPath(NSIndexPath(forRow: 0, inSection: 1), animated: true, scrollPosition: .None)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+
+    //cellのサイズを画面幅/3に設定する
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let cellSize = CGSize(width: collectionView.frame.width/3, height: 50)
+        return cellSize
+    }
+    
+    //cellタップ時の処理
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        println("tapped#:\(indexPath.section)")
+
+        if indexPath.section == collectionView.numberOfSections()-2{
+            
+        }else{
+            collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: .CenteredHorizontally, animated:true)
+        }
+        
     }
 
-
+    
 }
 
